4793150b8e40a71817ad50cd888fd6ece363af6b
dianxiap <1207864396@qq.com>
2023-07-18 20:56:23 +0800

    list测试

---
+++ Project52/Project52/源.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <list>
+using namespace std;
+
+﻿int main()
+{
+
+	int array[] = { 1, 2, 3, 4, 0, 5, 6, 7, 8, 9 };
+
+	int n = sizeof(array) / sizeof(int);
+
+	list<int> mylist(array, array + n);
+
+	auto it = mylist.begin();
+
+	while (it != mylist.end())
+
+	{
+
+		if (*it != 0)
+
+			cout << *it << " ";
+
+		else
+
+			it = mylist.erase(it);
+
+		++it;
+
+	}
+
+	return 0;
+
+}
